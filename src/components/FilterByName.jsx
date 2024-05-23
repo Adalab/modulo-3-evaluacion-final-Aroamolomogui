4eca79d1c6700957010556218f7e7dd917602d4d
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-23 14:06:07 +0200

    add to preventDefault in form and control value name input type text

--- src/components/FilterByName.jsx
+++ src/components/FilterByName.jsx
@@ -2,12 +2,18 @@
 function FilterByName({setName,name}) {
 
   const handleChangeInput = (ev)=>{
+    //cuando cambio cojo lo que es usuario escribe//
     setName(ev.target.value)
+    } 
+    const handleClick = ()=> {
+      //quiero que ese bntn al darle a reset me deje el input name en vacio//
+      setName ("")
   }
   return <>
-    <form action="">
-      <input type="text" name="name" id="name" onChange={handleChangeInput}/>
-      <input type="submit"  value="Reset" />
+  
+    <form onSubmit={(ev) => {ev.preventDefault() }}>
+      <input type="text" name="name" id="name" onChange={handleChangeInput} value={name}/>
+      <input type="submit"  value="Reset" onClick= {handleClick} />
     </form>
     <p> Hey! Encontraste a {name}</p>
   </>;
