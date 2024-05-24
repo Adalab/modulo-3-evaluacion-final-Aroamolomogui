5f1ff2cb4675dd9926c9112c3782f67d59037eb8
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-24 13:42:33 +0200

    add finish

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,12 +1,15 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Details({characterDetail}) {
   return <div>
     <img src= {characterDetail.image} alt="" />
         <h3>{characterDetail.name}</h3>
         <p>{characterDetail.species}</p>
+       
   </div>;
 }
+
 
 export default Details;
